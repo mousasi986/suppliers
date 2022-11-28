58eab5a0fbb00f11d977a86979521f8012d83e68
Aleksandr Mangus <eterlate@gmail.com>
2022-11-28 15:09:24 +0600

    header added

--- client/src/partials/Header.tsx
+++ client/src/partials/Header.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header>Header</header>
+      <header>
+        <div className='logo'>
+          <h1>Suppliers</h1>
+        </div>
+        <div className='main'>
+          <a>Норм</a>
+          <a>Норм</a>
+          <a>Норм</a>
+        </div>
+        <div className='header_auth'>
+          <a>Вход</a>
+          <a>Выход</a>
+        </div>
+      </header>
   )
 }
 
