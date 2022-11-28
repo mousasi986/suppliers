532e82c8cd2d50dd06d532e2fdb8c691e6511336
mousasi986 <59756997+mousasi986@users.noreply.github.com>
2022-11-28 11:46:58 +0600

    ыыы

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.scss';
+import Router from './router/Router';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
